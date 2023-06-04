827ac60936920ef75ba6c0127e161823e1ffb229
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-06-04 12:41:29 -0400

    modificaciones

---
+++ js/proyecto.js
@@ -0,0 +1,18 @@
+function updateCounter() {
+    var countDate = new Date("June 06, 2023 00:00:00").getTime();
+    var now = new Date().getTime();
+    var difference = countDate - now;
+  
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  }
+  
+  setInterval(updateCounter, 1000);
+  
