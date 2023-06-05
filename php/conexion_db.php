49fa96263022a4ff5d5784cec7c6c2dff45c13e6
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-06-05 00:27:20 -0400

    avances

--- php/conexion_db.php
+++ php/conexion_db.php
@@ -1,12 +1,12 @@
 <?php
     $conexion = mysqli_connect("localhost","root", "", "catcoin");
-    /*
+    
     if($conexion){
         echo "Conexion exitosa";
     }else{
         echo "No se ha podido conectar a la bd";
     }
-    */
+    
 
 ?>
 
