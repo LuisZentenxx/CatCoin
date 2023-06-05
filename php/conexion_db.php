74bc6f4ee891fcf573b423a65bc4384a0d3f96fb
Barbarita <89460645+BarbaraCarvajal@users.noreply.github.com>
2023-06-05 00:48:28 -0400

    Revert "c:"

--- php/conexion_db.php
+++ php/conexion_db.php
@@ -1,6 +1,6 @@
 <?php
-    $conexion = mysqli_connect("localhost","root", "", "catcoin");
-    /*
+    $conexion = mysqli_connect("localhost","root", "", "catcoin", "3307");
+    
     if($conexion){
         echo "Conexion exitosa";
     }else{
