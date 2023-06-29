6349b1b75cecbcf5405b814d4d38ebb7c58e1b2e
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-06-29 01:58:24 -0400

    orden codigo principal y Js

--- js/registrar.js
+++ js/registrar.js
@@ -1,40 +1,70 @@
+/**
+ * Alternar la visibilidad de la contraseña en un campo de entrada de texto.
+ * @param {string} inputId - ID del elemento de entrada de texto.
+ * @param {string} iconId - ID del ícono asociado.
+ */
 function togglePasswordVisibility(inputId, iconId) {
+    // Obtener los elementos de entrada de texto y el ícono correspondiente mediante los IDs proporcionados.
     const input = document.getElementById(inputId);
     const icon = document.getElementById(iconId);
-    
+
+    // Verificar si el tipo de entrada es "password".
     if (input.type === "password") {
+        // Si es "password", cambiar el tipo de entrada a "text".
         input.type = "text";
+        // Actualizar las clases del ícono para mostrar el ícono de "ojo tachado".
         icon.classList.remove("bi-eye-fill");
         icon.classList.add("bi-eye-slash-fill");
     } else {
+        // Si no es "password" (es decir, es "text" u otro tipo), cambiar el tipo de entrada a "password".
         input.type = "password";
+        // Actualizar las clases del ícono para mostrar el ícono de "ojo".
         icon.classList.remove("bi-eye-slash-fill");
         icon.classList.add("bi-eye-fill");
     }
 }
 
+/**
+ * Alternar la visibilidad de la contraseña en un campo de entrada de texto al hacer clic en un botón.
+ * @param {string} inputId - ID del elemento de entrada de texto.
+ * @param {string} buttonId - ID del botón asociado.
+ */
 function togglePasswordVisibility(inputId, buttonId) {
+    // Obtener el elemento de entrada de texto y el botón correspondiente mediante los IDs proporcionados.
     var passwordInput = document.getElementById(inputId);
     var passwordButton = document.getElementById(buttonId);
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      passwordButton.classList.remove("bi-eye-fill");
-      passwordButton.classList.add("bi-eye-slash-fill");
-    } else {
-      passwordInput.type = "password";
-      passwordButton.classList.remove("bi-eye-slash-fill");
-      passwordButton.classList.add("bi-eye-fill");
-    }
-  }
 
-  function checkPasswordMatch() {
+    // Verificar si el tipo de entrada es "password".
+    if (passwordInput.type === "password") {
+        // Si es "password", cambiar el tipo de entrada a "text".
+        passwordInput.type = "text";
+        // Actualizar las clases del botón para mostrar el ícono de "ojo tachado".
+        passwordButton.classList.remove("bi-eye-fill");
+        passwordButton.classList.add("bi-eye-slash-fill");
+    } else {
+        // Si no es "password" (es decir, es "text" u otro tipo), cambiar el tipo de entrada a "password".
+        passwordInput.type = "password";
+        // Actualizar las clases del botón para mostrar el ícono de "ojo".
+        passwordButton.classList.remove("bi-eye-slash-fill");
+        passwordButton.classList.add("bi-eye-fill");
+    }
+}
+
+/**
+ * Verificar si dos contraseñas ingresadas coinciden y mostrar un mensaje correspondiente.
+ */
+function checkPasswordMatch() {
+    // Obtener los valores de las contraseñas ingresadas y el elemento de ayuda correspondiente.
     var password1 = document.getElementById("exampleInputPassword1").value;
     var password2 = document.getElementById("exampleInputPassword2").value;
     var confirmPasswordHelp = document.getElementById("confirmPasswordHelp");
 
+    // Verificar si las contraseñas coinciden.
     if (password1 === password2) {
-      confirmPasswordHelp.textContent = "Las contraseñas coinciden.";
+        // Si coinciden, mostrar un mensaje indicando que las contraseñas coinciden.
+        confirmPasswordHelp.textContent = "Las contraseñas coinciden.";
     } else {
-      confirmPasswordHelp.textContent = "Las contraseñas no coinciden.";
+        // Si no coinciden, mostrar un mensaje indicando que las contraseñas no coinciden.
+        confirmPasswordHelp.textContent = "Las contraseñas no coinciden.";
     }
-  }
+}
--- princi.php
+++ princi.php
@@ -10,40 +10,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CatCoin</title>
+
+    <!-- Fuentes Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Delicious+Handrawn&family=Roboto:ital,wght@1,300&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
+
+    <!-- Estilos CSS -->
     <link rel="stylesheet" href="css\princi.css">
 
+    <!-- Iconos Font Awesome -->
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
 </head>
 
 <body id="body">
 
+    <!-- Titulo bienvenida -->
     <div class="titulo">
         <h1 class="bienvenida">
             Bienvenid@ <?php echo $nombre_apellido; ?>
         </h1>
     </div>
 
+    <!-- Menu Lateral -->
     <div class="menu__side" id="menu_side">
 
+        <!-- Logo y nombre de la pagina -->
         <div class="name__page">
             <i class="fa-solid fa-cat"></i>
             <h4>CatCoin</h4>
         </div>
 
+        <!-- Opciones menú -->
         <div class="options__menu">
 
+            <!-- Icono abrir menú -->
             <a href="#" class="selected">
                 <div class="option">
                     <i class="fa-solid fa-arrow-right" id="btn_open"></i>
                 </div>
             </a>
 
-
+            <!-- Icono y Nombre Usuario -->
             <a href="#">
                 <div class="option">
                     <i class="fa-solid fa-user" title="Usuario"></i>
@@ -51,6 +61,7 @@
                 </div>
             </a>
 
+            <!-- Icono Estadisticas -->
             <a href="#">
                 <div class="option">
                     <i class="fa-solid fa-chart-simple"></i>
@@ -58,6 +69,7 @@
                 </div>
             </a>
 
+            <!-- Icono Gastos -->
             <a href="#">
                 <div class="option">
                     <i class="fa-solid fa-wallet"></i>
@@ -65,13 +77,15 @@
                 </div>
             </a>
 
+            <!-- Icono y enlace a Nosotros.php -->
             <a href="nosotros.php">
                 <div class="option">
-                <i class="fa-solid fa-people-group"></i>
+                    <i class="fa-solid fa-people-group"></i>
                     <h4>Nosotros</h4>
                 </div>
             </a>
 
+            <!-- Icono de Cerrar Sesión -->
             <a href="php/cerrar_sesion.php">
                 <div class="option">
                     <i class="fa-solid fa-right-from-bracket"></i>
@@ -82,49 +96,63 @@
         </div>
     </div>
 
+    <!-- Card Ingreso Presupuesto -->
     <div class="card" style="width: 30rem;">
         <div class="card-body">
-            <div class="col-md-6">
-                <h4 class="text-start">Ingresar Presupuesto</h4>
-                <form action="php/guardar_presupuesto.php" method="POST" class="text-start">
-                    <div class="mb-3">
-                        <i class="fa-solid fa-hand-holding-dollar"></i>
-                        <input type="number" name="monto" id="monto" class="form-control form-control-lg"
-                            value="<?php echo $placeholder_valor; ?>" required placeholder="$0">
-                    </div>
-                </form>
-            </div>
+            <div class="col-md-6"></div>
+            <h4 class="text-start">Ingresar Presupuesto</h4>
+
+            <!-- Formulario -->
+            <form action="php/guardar_presupuesto.php" method="POST" class="text-start">
+                <div class="mb-3">
+                    <i class="fa-solid fa-hand-holding-dollar"></i>
+                    <input type="number" name="monto" id="monto" class="form-control form-control-lg"
+                        value="<?php echo $placeholder_valor; ?>" required placeholder="$0">
+                </div>
+            </form>
+
         </div>
     </div>
+    </div>
 
+    <!-- Contenedor Card Formulario Gastos y Categoria de Gastos -->
     <div class="card-container">
+        <!-- Card Gastos -->
         <div class="card1" style="width: 30rem;">
             <div class="card-body">
                 <div class="col-md-6">
                     <h4 class="text-start">Ingresar Gastos</h4>
+
+                    <!-- Formulario Gastos -->
                     <form action="php/guardar_gasto.php" method="POST" id="myForm">
+
+                        <!-- Titulo gasto -->
                         <div class="mb-3">
                             <label for="titulo" class="form-label">Titulo</label>
                             <input type="text" name="titulo" id="titulo" class="form-control" required
                                 placeholder="Ingresa un titulo">
                         </div>
 
+                        <!-- Valor gasto -->
                         <div class="mb-3">
-                            <label for="valor" class="form-label">Valor</label>
-                            <input type="number" name="valor" id="valor" class="form-control" required placeholder="$0">
+                            <labelfor="valor" class="form-label">Valor</label>
+                                <input type="number" name="valor" id="valor" class="form-control" required
+                                    placeholder="$0">
                         </div>
 
+                        <!-- Desc. gasto -->
                         <div class="mb-3">
                             <label for="descripcion" class="form-label">Descripcion</label>
                             <input type="text" name="descripcion" id="descripcion" class="form-control" required>
                         </div>
 
-
+                        <!-- Fecha gasto -->
                         <div class="mb-3">
                             <label for="fecha" class="form-label">Fecha</label>
                             <input type="date" name="fecha" id="fecha" class="form-control" required>
                         </div>
 
+                        <!-- Categoría gasto -->
                         <div class="mb-3">
                             <label for="categoria" class="form-label">Categoría</label>
                             <select name="categoria" id="categoria" class="form-select">
@@ -135,27 +163,35 @@
                                 <option value="5">Otro</option>
                             </select>
                         </div>
+
+                        <!-- Botón Guardar -->
                         <input class="btn btn-primary" type="submit" name="submit" value="Guardar"></input>
                     </form>
                 </div>
             </div>
         </div>
 
+        <!-- Card Categorías-->
         <div class="card2" style="width: 40rem;">
             <div class="card-body">
                 <div class="col-md-6">
                     <h4 class="text-start">Gastos por categoría</h4>
                     <?php include 'php/gastos_categoria.php'; ?>
+
+                    <!-- Total gastos -->
                     <div class="total-gastos">
                         <p>Total</p>
                         <p>$<?php echo $sumaTotalGastos; ?></p>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
 
+
     <hr>
+    <!-- Sección Estadisticas -->
     <div>
         <h1>Estadísticas</h1>
         <div class="card3" style="width: 30rem;">
@@ -167,6 +203,7 @@
         </div>
     </div>
 
+    <!-- Graficos Google Charts -->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
     google.charts.load('current', {
@@ -192,6 +229,8 @@
         chart.draw(data, options);
     }
     </script>
+
+    <!-- Script Lógica -->
     <script src="js\script.js"></script>
 
     <!-- jQuery -->
